f80ab0311dbce4a0d53724a20c659b324639dac9
harryninja <harold_illert@outlook.com>
2023-09-20 07:30:23 -0300

    create

---
+++ src/app/pages/contacts/usergrid/data.ts
@@ -0,0 +1,65 @@
+const userGridData = [
+    {
+        id: 1,
+        name: 'Harry illert',
+        designation: 'UI/UX Designer',
+        projects: ['Photoshop', 'illustrator'],
+        email: 'david@frieza.com'
+    },
+    {
+        id: 2,
+        image: 'assets/images/users/avatar-2.jpg',
+        name: 'Frank Kirk',
+        designation: 'Frontend Developer',
+        projects: ['Html', 'Css', '2 + more'],
+        email: 'frank@frieza.com'
+    },
+    {
+        id: 3,
+        image: 'assets/images/users/avatar-3.jpg',
+        name: 'Rafael Morales',
+        designation: 'Backend Developer',
+        projects: ['Php', 'Java', 'Python'],
+        email: 'Rafael@frieza.com'
+    },
+    {
+        id: 4,
+        name: 'Mark Ellison',
+        designation: 'Full Stack Developer',
+        projects: ['Ruby', 'Php', '2 + more'],
+        email: 'mark@frieza.com'
+    },
+    {
+        id: 5,
+        image: 'assets/images/users/avatar-4.jpg',
+        name: 'Minnie Walter',
+        designation: 'Frontend Developer',
+        projects: ['Html', 'Css', '2 + more'],
+        email: 'minnie@frieza.com'
+    },
+    {
+        id: 6,
+        image: 'assets/images/users/avatar-5.jpg',
+        name: 'Shirley Smith',
+        designation: 'UI/UX Designer',
+        projects: ['Photoshop', 'illustrator'],
+        email: 'shirley@frieza.com'
+    },
+    {
+        id: 7,
+        name: 'John Santiago',
+        designation: 'Full Stack Developer',
+        projects: ['Ruby', 'Php', '2 + more'],
+        email: 'john@frieza.com'
+    },
+    {
+        id: 8,
+        image: 'assets/images/users/avatar-5.jpg',
+        name: 'Colin Melton',
+        designation: 'Backend Developer',
+        projects: ['Php', 'Java', 'Python'],
+        email: 'colin@frieza.com'
+    },
+];
+
+export { userGridData };
